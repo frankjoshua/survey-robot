d2e4be412106e646d267c65421af9154d4411253
josh <josh@tesseractmobile.com>
2019-11-27 01:24:46 -0600

    Added flash tools

---
+++ flash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./tools/flash.sh --bootconf ./rpi_boot_files/config.txt --userdata ./rpi_boot_files/user-data https://github.com/hypriot/image-builder-rpi/releases/download/v1.11.4/hypriotos-rpi-v1.11.4.img.zip
